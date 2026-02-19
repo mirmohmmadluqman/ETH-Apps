182d92511bf402ab9e432b3ac5b41819af2c5f1f
Mir Mohmmad Luqman <mirmohmmadluqman@gmail.com>
2026-02-19 22:21:15 +0530

    Update Multi-sig.sol

--- Multi-sig Wallet/src/Multi-sig.sol
+++ Multi-sig Wallet/src/Multi-sig.sol
@@ -1,57 +1,36 @@
 // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.13;
-
-// contract MultiSigWallet{
-//     event Deposit();
-//     event SubmitTransaction();
-//     event ConfirmTransaction();
-//     event RevokeConfirmation();
-//     event ExecuteTransaction();
-
-//     address[] public owners;
-//     mapping(address => bool) public isOwner;
-//     uint256 public numConfirmationsRequired;
-
-//     struct Transaction{
-//         address to;
-//         uint256 value;
-//         bytes data;
-//         bool executed;
-//         uint256 numConfirmations;
-//     }
-
-//     mapping(uint256 => mapping(address => bool)) public isConfirmed; // mapping from tx index => owner => bool
-//     Transaction[] public transactions;
-
-//     modifier onlyOwner(){
-//         require(isOwner[msg.sender], "not owner");
-//         _;
-//     }
-
-
-// }
-
 pragma solidity ^0.8.13;
 
 contract MultiSigWallet{
-    // State variables
-    uint256 private _requiredSignature;
-    address[] private _owners;
+    event Deposit();
+    event SubmitTransaction();
+    event ConfirmTransaction();
+    event RevokeConfirmation();
+    event ExecuteTransaction();
+
+    address[] public owners;
+    mapping(address => bool) public isOwner;
+    uint256 public numConfirmationsRequired;
 
     struct Transaction{
-    address to;
-    uint256 value;
-    bytes data;
-    bool executed;
-    mapping(address => bool) signatures;
+        address to;
+        uint256 value;
+        bytes data;
+        bool executed;
+        uint256 numConfirmations;
     }
 
-    Transaction[] private _transactions;    
+    mapping(uint256 => mapping(address => bool)) public isConfirmed; // mapping from tx index => owner => bool
+    Transaction[] public transactions;
 
-    // Events
-    event TransactionCreated(uint256 transactionId, address to, uint256 value, bytes data);
-    event TransactionSigned(uint256 transactionId, address signer);
-    event TransactionExecuted(uint256 transactionId, address executer);
+    modifier onlyOwner(){
+        require(isOwner[msg.sender], "not owner");
+        _;
+    }
 
-    
+    modifier notExecuted(uint256 _txIndex) {
+        require(!transactions[_txIndex].executed, "tx already executed");
+        _;
+    }
 }
+
